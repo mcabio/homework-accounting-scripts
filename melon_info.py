6b47627c8ce0a19b5e0471d731480712f301abc0
Meryl Lou Cabio <meryllou7@gmail.com>
2023-12-04 22:39:47 -0600

    Completed homework and added notes

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,31 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+from melons import all_melons
 
-
-def print_melon(name, price, seedless):
+def print_melons(all_melons):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+#  all_melons.items() returns a view of the dictionary's items as a list of tuples. 
+# Each tuple consists of a key-value pair, where melon_name is assigned to the KEY, 
+# and attributes is assigned the value. In this case, the values contain the attributes for that melon.
+# After 'for'...the variable that is used will become the KEY of that dictionary, in this case it's melon_name
+    for melon_name, attributes in all_melons.items(): 
+        print(melon_name.upper())
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
+# This for loop iterates through the items of the attributes dictionary, 
+# which contains the attributes for a specific melon.
+# attributes.items(): returns a view of the dictionary's items as a list of tuples. 
+# Each tuple consists of a key-value pair, where the key is an attribute name, 
+# and the value is the corresponding attribute value.
+# In the key:value pair format, attribute would be key. Value would be value
+# The print statement prints each attribute and its corresponding value in a formatted string.
+# This then removes the curly brackets.
+        for attribute, value in attributes.items():
+            print(f'{attribute}: {value}')
+
+        print('===================================')
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+print_melons(all_melons)
+
--- melons.py
+++ melons.py
@@ -1,23 +1,64 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
+
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
+
+# CASABA
+# price: 2.5
+# seedless: False
+# flesh_color: None
+# rind_color: None
+# average_weight: None
+
+all_melons = {
+    'HONEYDEW': {'price' : '0.99',
+                  'seedless' : True,
+                  'flesh_color' : None,
+                  'rind_color' : None,
+                  'average_weight' : None
+                  },
+    'CRENSHAW': {'price': '2.00',
+                  'seedless' : False,
+                  'flesh_color': None,
+                  'rind_color' : None,
+                  'average_weight' : None
+                  },
+    'CRANE': {'price': '2.50',
+               'seedless': False,
+               'flesh_color': None,
+               'rind_color': None,
+               'average_weight': None
+               },
+    'CASABA': {'price': '2.50',
+               'seedless': False,
+               'flesh_color': None,
+               'rind_color': None,
+               'average_weight': None
+               },
+    'CANTALOUPE': {'price': '0.99',
+                   'seedless': False,
+                   'flesh_color': None,
+                   'rind_color': None,
+                   'average_weight': None}
 }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
